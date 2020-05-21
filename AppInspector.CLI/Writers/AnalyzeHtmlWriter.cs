15dad917919f3201690428806681dbfc708d4153
Gabe Stocco <98900+gfs@users.noreply.github.com>
2020-05-21 13:07:10 -0700

    Remove unused dependency.

--- AppInspector.CLI/Writers/AnalyzeHtmlWriter.cs
+++ AppInspector.CLI/Writers/AnalyzeHtmlWriter.cs
@@ -7,7 +7,6 @@ using Microsoft.ApplicationInspector.Commands;
 using Microsoft.ApplicationInspector.RulesEngine;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
