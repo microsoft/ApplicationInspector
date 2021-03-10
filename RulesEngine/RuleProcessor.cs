342c83171d340834b0280152fb9a5f8bd0302004
Gabe Stocco <98900+gfs@users.noreply.github.com>
2021-03-10 13:13:04 -0800

    Whitespace

--- RulesEngine/RuleProcessor.cs
+++ RulesEngine/RuleProcessor.cs
@@ -262,7 +262,6 @@ namespace Microsoft.ApplicationInspector.RulesEngine
                         break;
                     }
                 }
-
             }
 
             if (removeOld && matchRecordToRemove != null)
