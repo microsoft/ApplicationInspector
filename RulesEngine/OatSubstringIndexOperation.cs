ae196b87ffd3354bccec4f1f01abc5c32963394c
Gabe Stocco <98900+gfs@users.noreply.github.com>
2021-05-12 04:04:22 -0700

    Use substring when possible for performance
    Seeing significant performance uplift.

---
+++ RulesEngine/OatSubstringIndexOperation.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.CST.OAT;
+using Microsoft.CST.OAT.Operations;
+using Microsoft.CST.OAT.Utils;
+using Serilog;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Microsoft.ApplicationInspector.RulesEngine
+{
+    /// <summary>
+    /// The Custom Operation to enable identification of pattern index in result used by Application Inspector to report why a given
+    /// result was matched and to retrieve other pattern level meta-data
+    /// </summary>
+    public class OATSubstringIndexOperation : OatOperation
+    {
+        /// <summary>
+        /// Create an OatOperation given an analyzer
+        /// </summary>
+        /// <param name="analyzer">The analyzer context to work with</param>
+        public OATSubstringIndexOperation(Analyzer analyzer) : base(Operation.Custom, analyzer)
+        {
+            CustomOperation = "SubstringIndex";
+            OperationDelegate = SubstringIndexOperationDelegate;
+            ValidationDelegate = SubstringIndexValidationDelegate;
+        }
+
+        public IEnumerable<Violation> SubstringIndexValidationDelegate(CST.OAT.Rule rule, Clause clause)
+        {
+            if (clause.Data?.Count == null || clause.Data?.Count == 0)
+            {
+                yield return new Violation(string.Format(Strings.Get("Err_ClauseNoData"), rule.Name, clause.Label ?? rule.Clauses.IndexOf(clause).ToString(CultureInfo.InvariantCulture)), rule, clause);
+            }
+            if (clause.DictData != null && clause.DictData?.Count > 0)
+            {
+                yield return new Violation(string.Format(Strings.Get("Err_ClauseDictDataUnexpected"), rule.Name, clause.Label ?? rule.Clauses.IndexOf(clause).ToString(CultureInfo.InvariantCulture), clause.Operation.ToString()), rule, clause);
+            }
+        }
+
+        /// <summary>
+        /// Returns results with pattern index and Boundary as a tuple to enable retrieval of Rule pattern level meta-data like Confidence and report the 
+        /// pattern that was responsible for the match
+        /// </summary>
+        /// <param name="clause"></param>
+        /// <param name="state1"></param>
+        /// <param name="state2"></param>
+        /// <param name="captures"></param>
+        /// <returns></returns>
+        public OperationResult SubstringIndexOperationDelegate(Clause clause, object? state1, object? state2, IEnumerable<ClauseCapture>? captures)
+        {
+            if (state1 is TextContainer tc && clause is OATSubstringIndexClause src)
+            {
+                if (clause.Data is List<string> stringList && stringList.Any())
+                {
+                    var outmatches = new List<(int, Boundary)>();//tuple results i.e. pattern index and where
+
+                    for (int i = 0; i < stringList.Count; i++)
+                    {
+                        var idx = 0;
+                        while (idx != -1)
+                        {
+                            idx = tc.FullContent.IndexOf(stringList[i], idx);
+                            if (idx != -1)
+                            {
+                                if (src.UseWordBoundaries)
+                                {
+                                    if (idx > 0 && !char.IsLetterOrDigit(tc.FullContent[idx - 1]))
+                                    {
+                                        continue;
+                                    }
+                                    if (idx + 1 < tc.FullContent.Length && !char.IsLetterOrDigit(tc.FullContent[idx + 1]))
+                                    {
+                                        continue;
+                                    }
+                                }
+                                Boundary newBoundary = new Boundary()
+                                {
+                                    Length = stringList[i].Length,
+                                    Index = idx
+                                };
+                                if (tc.ScopeMatch(src.Scopes, newBoundary))
+                                {
+                                    outmatches.Add((i, newBoundary));
+                                }
+                            }
+                        }
+                    }
+
+                    var result = src.Invert ? outmatches.Count == 0 : outmatches.Count > 0;
+                    return new OperationResult(result, result && src.Capture ? new TypedClauseCapture<List<(int, Boundary)>>(clause, outmatches, state1) : null);
+                }
+            }
+            return new OperationResult(false, null);
+        }
+    }
+}
