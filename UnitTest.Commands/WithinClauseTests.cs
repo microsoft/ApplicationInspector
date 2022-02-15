4801c04e0511d76f21684a271b4c2dc126ee55db
Gabe Stocco <98900+gfs@users.noreply.github.com>
2022-02-15 14:34:00 -0800

    More new precise tests for within clause

--- UnitTest.Commands/WithinClauseTests.cs
+++ UnitTest.Commands/WithinClauseTests.cs
@@ -11,23 +11,88 @@
     [TestClass]
     public class WithinClauseTests
     {
-        [TestMethod]
-        public void WithinClauseWithNoMatches()
+        
+
+        [DataRow("WithinClauseWithInvertWithFindingRange")]
+        [DataRow("WithinClauseWithoutInvertWithFindingRange")]
+        [DataRow("WithinClauseWithInvertWithSameLine")]
+        [DataRow("WithinClauseWithoutInvertWithSameLine")]
+        [DataTestMethod]
+        public void WithinClauseInvertTest(string testDataKey)
         {
-            string testData = @"#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+            WithinClauseInvertTest(testData[testDataKey].testData, testData[testDataKey].conditionRegion, testData[testDataKey].negate, testData[testDataKey].expectedNumMatches, testData[testDataKey].expectedMatchesLineStarts);
+        }
 
-#define BUFSIZER1 512
-#define BUFSIZER2 ((BUFSIZER1 / 2) - 8)
+        internal void WithinClauseInvertTest(string testData, string condition_region, bool invert, int expectedMatches, int[] expectedMatchesLineStarts)
+        {
+            RuleSet rules = new(null);
+            var newRule = baseRule.Replace("REPLACE_REGION", condition_region).Replace("REPLACE_NEGATE", invert.ToString().ToLowerInvariant());
+            rules.AddString(newRule, "TestRules");
+            RuleProcessor processor = new(rules, new RuleProcessorOptions());
+            if (Language.FromFileNameOut("test.c", out LanguageInfo info))
+            {
+                List<MatchRecord> matches = processor.AnalyzeFile(testData, new Microsoft.CST.RecursiveExtractor.FileEntry("test.cs", new MemoryStream()), info);
+                Assert.AreEqual(expectedMatches, matches.Count);
+                foreach (int expectedMatchLineStart in expectedMatchesLineStarts)
+                {
+                    MatchRecord? correctLineMatch = matches.FirstOrDefault(match => match.StartLocationLine == expectedMatchLineStart);
+                    Assert.IsNotNull(correctLineMatch);
+                    matches.Remove(correctLineMatch);
+                }
+            }
+            else
+            {
+                Assert.Fail();
+            }
+        }
 
-int main(int argc, char **argv)
-{
-    char *buf1R1;
-    char *buf2R1;
-}";
-            string testRules = @"[
+        [DataRow(true, 1, new int[] { 2 })]
+        [DataRow(false, 1, new int[] { 3 })]
+        [DataTestMethod]
+        public void WithinClauseInvertTestForSameLine(bool invert, int expectedMatches, int[] expectedMatchesLineStarts)
+        {
+            RuleSet rules = new(null);
+            var newRule = findingOnlyRule.Replace("REPLACE_NEGATE", invert.ToString().ToLowerInvariant());
+            rules.AddString(newRule, "TestRules");
+            RuleProcessor processor = new(rules, new RuleProcessorOptions());
+            if (Language.FromFileNameOut("test.c", out LanguageInfo info))
+            {
+                List<MatchRecord> matches = processor.AnalyzeFile(insideFindingData, new Microsoft.CST.RecursiveExtractor.FileEntry("test.cs", new MemoryStream()), info);
+                Assert.AreEqual(expectedMatches, matches.Count);
+                foreach (int expectedMatchLineStart in expectedMatchesLineStarts)
+                {
+                    MatchRecord? correctLineMatch = matches.FirstOrDefault(match => match.StartLocationLine == expectedMatchLineStart);
+                    Assert.IsNotNull(correctLineMatch);
+                    matches.Remove(correctLineMatch);
+                }
+            }
+            else
+            {
+                Assert.Fail();
+            }
+        }
+
+        Dictionary<string, (string testData, string conditionRegion, bool negate, int expectedNumMatches, int[] expectedMatchesLineStarts)> testData = new()
+        {
+            {
+                "WithinClauseWithInvertWithFindingRange",
+                (findingRangeData, "finding-region(0,1)", true, 1, new int[] { 13 })
+            },
+            {
+                "WithinClauseWithoutInvertWithFindingRange",
+                (findingRangeData, "finding-region(0,1)", false, 1, new int[] { 14 })
+            },
+            {
+                "WithinClauseWithInvertWithSameLine",
+                (sameLineData, "same-line", true, 1, new int[] { 13 })
+            },
+            {
+                "WithinClauseWithoutInvertWithSameLine",
+                (sameLineData, "same-line", false, 1, new int[] { 14 })
+            }
+        };
+
+        private const string findingOnlyRule = @"[
     {
         ""id"": ""SA000005"",
         ""name"": ""Testing.Rules.MallocNotFree1"",
@@ -38,7 +103,7 @@ int main(int argc, char **argv)
         ""description"": ""this rule aims to find malloc() that does NOT have free() in 1 line range"",
         ""patterns"": [
             {
-                ""pattern"": ""malloc\\("",
+                ""pattern"": ""racecar"",
                 ""type"": ""regex"",
                 ""confidence"": ""High"",
                 ""modifiers"": [
@@ -52,117 +117,25 @@ int main(int argc, char **argv)
         ""conditions"": [
             {
                 ""pattern"": {
-                    ""pattern"": ""free\\("",
+                    ""pattern"": ""car"",
                     ""type"": ""regex"",
                     ""scopes"": [
                         ""code""
-                    ],
-                    ""modifiers"": [
-                        ""i""
                     ]
                 },
-                ""search_in"": ""finding-region(0,1)"",
-                ""negate_finding"": true
+                ""search_in"": ""finding-only"",
+                ""negate_finding"": REPLACE_NEGATE
             }
         ],
         ""_comment"": """"
     }
 ]";
-            RuleSet rules = new(null);
-            rules.AddString(testRules, "WithinClauseWithInverTestRules");
-            RuleProcessor processor = new(rules, new RuleProcessorOptions());
-            if (Language.FromFileNameOut("test.cs", out LanguageInfo info))
-            {
-                List<MatchRecord> matches = processor.AnalyzeFile(testData, new Microsoft.CST.RecursiveExtractor.FileEntry("test.cs", new MemoryStream()), info);
-                Assert.AreEqual(0, matches.Count);
-            }
-            else
-            {
-                Assert.Fail();
-            }
-        }
 
-        [TestMethod]
-        public void WithinClauseWithInvert()
-        {
-            string testData = @"#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+        const string insideFindingData = @"
+raceCAR
+racecar";
 
-#define BUFSIZER1 512
-#define BUFSIZER2 ((BUFSIZER1 / 2) - 8)
-
-int main(int argc, char **argv)
-{
-    char *buf1R1;
-    char *buf2R1;
-    buf1R1 = (char *)malloc(BUFSIZER1);
-    buf2R1 = (char *)malloc(BUFSIZER1);
-    free(buf2R1);
-    strncpy(buf2R1, argv[1], BUFSIZER1 - 1);
-    free(buf1R1);
-}";
-            string testRules = @"[
-    {
-        ""id"": ""SA000005"",
-        ""name"": ""Testing.Rules.MallocNotFree1"",
-        ""tags"": [
-            ""Testing.Rules.MallocNotFree1""
-        ],
-        ""severity"": ""Critical"",
-        ""description"": ""this rule aims to find malloc() that does NOT have free() in 1 line range"",
-        ""patterns"": [
-            {
-                ""pattern"": ""malloc\\("",
-                ""type"": ""regex"",
-                ""confidence"": ""High"",
-                ""modifiers"": [
-                    ""i""
-                ],
-                ""scopes"": [
-                    ""code""
-                ]
-            }
-        ],
-        ""conditions"": [
-            {
-                ""pattern"": {
-                    ""pattern"": ""free\\("",
-                    ""type"": ""regex"",
-                    ""scopes"": [
-                        ""code""
-                    ],
-                    ""modifiers"": [
-                        ""i""
-                    ]
-                },
-                ""search_in"": ""finding-region(0,1)"",
-                ""negate_finding"": true
-            }
-        ],
-        ""_comment"": """"
-    }
-]";
-            RuleSet rules = new(null);
-            rules.AddString(testRules, "WithinClauseWithInverTestRules");
-            RuleProcessor processor = new(rules, new RuleProcessorOptions());
-            if (Language.FromFileNameOut("test.cs", out LanguageInfo info))
-            {
-                List<MatchRecord> matches = processor.AnalyzeFile(testData, new Microsoft.CST.RecursiveExtractor.FileEntry("test.cs", new MemoryStream()), info);
-                Assert.AreEqual(1, matches.Count);
-                Assert.AreEqual(13, matches[0].StartLocationLine);
-            }
-            else
-            {
-                Assert.Fail();
-            }
-        }
-
-        [TestMethod]
-        public void WithinClauseWithoutInvert()
-        {
-            string testData = @"#include <stdio.h>
+        const string findingRangeData = @"#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
@@ -180,7 +153,26 @@ int main(int argc, char **argv)
     strncpy(buf2R1, argv[1], BUFSIZER1 - 1);
     free(buf1R1);
 }";
-            string testRules = @"[
+
+        const string sameLineData = @"#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define BUFSIZER1 512
+#define BUFSIZER2 ((BUFSIZER1 / 2) - 8)
+
+int main(int argc, char **argv)
+{
+    char *buf1R1;
+    char *buf2R1;
+    buf1R1 = (char *)malloc(BUFSIZER1);
+    buf2R1 = (char *)malloc(BUFSIZER1);free(buf2R1);
+    
+    strncpy(buf2R1, argv[1], BUFSIZER1 - 1);
+    free(buf1R1);
+}";
+        private const string baseRule = @"[
     {
         ""id"": ""SA000005"",
         ""name"": ""Testing.Rules.MallocNotFree1"",
@@ -214,26 +206,12 @@ int main(int argc, char **argv)
                         ""i""
                     ]
                 },
-                ""search_in"": ""finding-region(0,1)"",
-                ""negate_finding"": false
+                ""search_in"": ""REPLACE_REGION"",
+                ""negate_finding"": REPLACE_NEGATE
             }
         ],
         ""_comment"": """"
     }
 ]";
-            RuleSet rules = new(null);
-            rules.AddString(testRules, "WithinClauseWithInverTestRules");
-            RuleProcessor processor = new(rules, new RuleProcessorOptions());
-            if (Language.FromFileNameOut("test.cs", out LanguageInfo info))
-            {
-                List<MatchRecord> matches = processor.AnalyzeFile(testData, new Microsoft.CST.RecursiveExtractor.FileEntry("test.cs", new MemoryStream()), info);
-                Assert.AreEqual(1, matches.Count);
-                Assert.AreEqual(14, matches[0].StartLocationLine);
-            }
-            else
-            {
-                Assert.Fail();
-            }
-        }
     }
 }
