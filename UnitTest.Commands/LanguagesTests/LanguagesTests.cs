8afe18a339cdc1fd7df1d63fa6d304c2f4c773bd
Gabe Stocco <98900+gfs@users.noreply.github.com>
2022-02-22 12:11:05 -0800

    Basic test case for loading a custom language and comments file.

---
+++ UnitTest.Commands/LanguagesTests/LanguagesTests.cs
@@ -0,0 +1,20 @@
+﻿namespace ApplicationInspector.Unitprocess.Language
+{
+    using ApplicationInspector.Unitprocess.Misc;
+    using Microsoft.ApplicationInspector.RulesEngine;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.IO;
+
+    [TestClass]
+    public class LanguagesTests
+    {
+        [TestMethod]
+        public void DetectCustomLanguage()
+        {
+            var languages = new Languages(Path.Combine(Helper.GetPath(Helper.AppPath.testSource), @"languageTestFiles\comments_z_only.json"), Path.Combine(Helper.GetPath(Helper.AppPath.testSource), @"languageTestFiles\languages_z_only.json"));
+            Assert.IsTrue(languages.FromFileNameOut("afilename.z", out var language));
+            Assert.AreEqual("z",language.Name);
+            Assert.IsFalse(languages.FromFileNameOut("afilename.c", out var language2));
+        }
+    }  
+}
