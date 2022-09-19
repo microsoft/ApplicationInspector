8253b723844e8f4610ca18651666ba35036f1175
Gabe Stocco <98900+gfs@users.noreply.github.com>
2022-09-19 10:49:37 -0700

    Add array slicing test

--- AppInspector.YamlPath.Tests/UnitTest1.cs
+++ AppInspector.YamlPath.Tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Linq;
 using YamlDotNet.RepresentationModel;
 
 namespace AppInspector.YamlPath.Tests;
@@ -9,20 +10,29 @@ public class UnitTest1
 {
     private const string testData = 
         @"some_array:
-    - element1
-    - element2
-    - element3
-    - element4";
+    - 0
+    - 1
+    - 2
+    - 3";
     
-    [TestMethod]
-    public void TestMethod1()
+    [DataRow("/some_array/1", 1, new int[]{1})]
+    [DataRow("/some_array/[1:2]", 1, new int[]{1})]
+    [DataRow("/some_array/[1:4]", 3, new int[]{1, 2, 3})]
+    [DataRow("/some_array/5", 0, new int[]{})]
+    [DataRow("/some_array/[0:4]", 4, new int[]{0, 1, 2, 3})]
+    [DataRow("/some_array/[-3:-2]", 1, new int[]{1})]
+
+    [DataTestMethod]
+    public void TestArraySlicing(string yamlPath, int expectedNumMatches, int[] expectedFindings)
     {
         var yaml = new YamlStream();
         yaml.Load(new StringReader(testData));
         var mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
-        var matching = mapping.YamlPathQuery("/some_array/1");
-        Console.WriteLine(matching.Count);
-        Console.WriteLine(matching.Count);
-
+        var matching = mapping.YamlPathQuery(yamlPath);
+        Assert.AreEqual(expectedNumMatches, matching.Count);
+        foreach (var expectedFinding in expectedFindings)
+        {
+            Assert.IsTrue(matching.Any(x => x is YamlScalarNode ysc && int.TryParse(ysc.Value, out int ysv) && ysv == expectedFinding));
+        }
     }
 }
