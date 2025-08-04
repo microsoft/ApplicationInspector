17e927ff1390c2417515e07434391e0ba81a2ea0
Giulia Stocco <98900+gfs@users.noreply.github.com>
2025-08-04 09:25:47 -0700

    Add a test case to replicate reported bug behavior
    Should pass when fixed

--- AppInspector.Tests/RuleProcessor/XmlAndJsonTests.cs
+++ AppInspector.Tests/RuleProcessor/XmlAndJsonTests.cs
@@ -843,4 +843,169 @@ test:
             Assert.Single(matches);
         }
     }
+
+    [Fact]
+    public void XPathComplexSampleExtractionBug()
+    {
+        // This test case reproduces the bug reported in https://github.com/microsoft/ApplicationInspector/issues/621
+        // The issue is that when using complex XPath expressions, the sample and excerpt returned are from the beginning 
+        // of the document instead of the matched content.
+        
+        var complexXPathRule = @"[
+    {
+        ""name"": ""TEST xpath complex"",
+        ""description"": ""Detects the use of 1.x version of mylibrary"",
+        ""id"": ""XPATH000001"",
+        ""applies_to"": [
+            ""pom.xml""
+        ],
+        ""tags"": [
+            ""XPATH.Version""
+        ],
+        ""severity"": ""critical"",
+        ""patterns"": [
+            {
+                ""pattern"": ""1\\..+"",
+                ""type"": ""regex"",
+                ""scopes"": [
+                    ""code""
+                ],
+                ""modifiers"": [],
+                ""confidence"": ""high"",
+                ""xpaths"": [""//*[local-name(.)='artifactId' and text()='mylibrary']/parent::*/*[local-name(.)='version']""]
+            }
+        ]
+    }
+]";
+
+        var complexPomXml = @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<project xmlns=""http://maven.apache.org/POM/4.0.0"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xsi:schemaLocation=""http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd"">
+  <modelVersion>4.0.0</modelVersion>
+
+  <artifactId>test</artifactId>
+
+  <dependencyManagement>
+    <dependencies>
+      <dependency>
+        <groupId>test.com</groupId>
+        <artifactId>mylibrary</artifactId>
+        <version>1.6.0</version>
+      </dependency>
+    </dependencies>
+  </dependencyManagement>
+
+</project>";
+
+        RuleSet rules = new();
+        rules.AddString(complexXPathRule, "XPathTestRules");
+        Microsoft.ApplicationInspector.RulesEngine.RuleProcessor processor = new(rules,
+            new RuleProcessorOptions { AllowAllTagsInBuildFiles = true });
+        
+        if (_languages.FromFileNameOut("pom.xml", out var info))
+        {
+            var matches = processor.AnalyzeFile(complexPomXml, new FileEntry("pom.xml", new MemoryStream()), info);
+            Assert.Single(matches);
+            
+            var match = matches[0];
+            
+            // Test for correct behavior (this will fail until the bug is fixed)
+            // BUG: https://github.com/microsoft/ApplicationInspector/issues/621
+            // When using complex XPath expressions, the sample should contain the actual matched content
+            // but currently returns content from the beginning of the document
+            
+            var expectedVersionPosition = complexPomXml.IndexOf("1.6.0");
+            Assert.True(expectedVersionPosition > 0, "Version should be found in the document");
+            
+            // These assertions test the correct behavior that should happen once the bug is fixed
+            Assert.Equal(expectedVersionPosition, match.Boundary.Index);
+            Assert.Equal(5, match.Boundary.Length); // Length of "1.6.0"
+            Assert.Equal("1.6.0", match.Sample);
+        }
+        else
+        {
+            Assert.Fail("Failed to get language info for pom.xml");
+        }
+    }
+
+    [Fact]
+    public void XPathComplexSampleExtractionBugStringPattern()
+    {
+        // This test case reproduces the second part of the bug reported in issue #621
+        // Testing with string pattern type instead of regex
+        
+        var complexXPathRuleString = @"[
+    {
+        ""name"": ""TEST xpath complex string"",
+        ""description"": ""Detects the use of 2.6.0 version of mylibrary"",
+        ""id"": ""XPATH000002"",
+        ""applies_to"": [
+            ""pom.xml""
+        ],
+        ""tags"": [
+            ""XPATH.Version""
+        ],
+        ""severity"": ""critical"",
+        ""patterns"": [
+            {
+                ""pattern"": ""2.6.0"",
+                ""type"": ""string"",
+                ""scopes"": [
+                    ""code""
+                ],
+                ""modifiers"": [],
+                ""confidence"": ""high"",
+                ""xpaths"": [""//*[local-name(.)='artifactId' and text()='mylibrary']/parent::*/*[local-name(.)='version']""]
+            }
+        ]
+    }
+]";
+
+        var complexPomXmlVersion2 = @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<project xmlns=""http://maven.apache.org/POM/4.0.0"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xsi:schemaLocation=""http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd"">
+  <modelVersion>4.0.0</modelVersion>
+
+  <artifactId>test</artifactId>
+
+  <dependencyManagement>
+    <dependencies>
+      <dependency>
+        <groupId>test.com</groupId>
+        <artifactId>mylibrary</artifactId>
+        <version>2.6.0</version>
+      </dependency>
+    </dependencies>
+  </dependencyManagement>
+
+</project>";
+
+        RuleSet rules = new();
+        rules.AddString(complexXPathRuleString, "XPathTestRules");
+        Microsoft.ApplicationInspector.RulesEngine.RuleProcessor processor = new(rules,
+            new RuleProcessorOptions { AllowAllTagsInBuildFiles = true });
+        
+        if (_languages.FromFileNameOut("pom.xml", out var info))
+        {
+            var matches = processor.AnalyzeFile(complexPomXmlVersion2, new FileEntry("pom.xml", new MemoryStream()), info);
+            Assert.Single(matches);
+            
+            var match = matches[0];
+            
+            // Test for correct behavior (this will fail until the bug is fixed)
+            // BUG: https://github.com/microsoft/ApplicationInspector/issues/621
+            // When using complex XPath expressions, the sample should contain the actual matched content
+            // but currently returns content from the beginning of the document
+            
+            var expectedVersionPosition = complexPomXmlVersion2.IndexOf("2.6.0");
+            Assert.True(expectedVersionPosition > 0, "Version should be found in the document");
+            
+            // These assertions test the correct behavior that should happen once the bug is fixed
+            Assert.Equal(expectedVersionPosition, match.Boundary.Index);
+            Assert.Equal(5, match.Boundary.Length); // Length of "2.6.0"
+            Assert.Equal("2.6.0", match.Sample);
+        }
+        else
+        {
+            Assert.Fail("Failed to get language info for pom.xml");
+        }
+    }
 }
